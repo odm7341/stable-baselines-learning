9842748b469d8eddfed0711e1f70543115e9395c
Owen M <omcclu@gmail.com>
2023-08-24 13:27:16 -0400

    more fixes

--- train_model.py
+++ train_model.py
@@ -4,17 +4,21 @@ from easy21 import Easy21
 env = Easy21()
 env.reset()
 
-model = A2C("MlpPolicy", env, verbose=1, device="cpu", tensorboard_log="./tb_logs/")
+model = PPO("MlpPolicy", env, verbose=1, device="cpu", tensorboard_log="./tb_logs/")
 
 model.learn(total_timesteps=10000)
 
-episodes = 30
+episodes = 100
 wins = 0
+total_ep_len = 0
 for episode in range(episodes):
     done = False
     obs, info = env.reset()
+    ep_len = 0
     while not done:
         env.render()
+        ep_len += 1
+        total_ep_len += 1
         print("obs:", obs)
         action, _states = model.predict(obs)
         #action = env.action_space.sample()
@@ -26,8 +30,11 @@ for episode in range(episodes):
         print("reward:", reward)
         if done:
             env.render()
+            print("Episode Length:", ep_len)
             if reward > 0:
                 wins += 1
+                print("WIN", end="")
             print("------")
-
+            
+print("Average Episode Length:", total_ep_len / episodes)
 print("Win Rate:", wins / episodes)
